6cf72f72e84628e0b06b926a2f2a755f139b30c6
Yash Mudgal <96115337+Mudgal2001@users.noreply.github.com>
2022-03-20 19:23:47 +0530

    added department check delete get

--- Backend/Routes/Auth.js
+++ Backend/Routes/Auth.js
@@ -179,5 +179,79 @@ router.delete("/deleteuser", async (req, res) => {
   }
   res.status(400).json({ result: "Userdeleted", userDeleted: olduser });
 });
+
+router.post("/checkdepartment",
+  [
+    body("email", "CheckEmail").isEmail(),
+    // body("password", "Password Must be atleast of 5 characters").isLength({
+    //   min: 1,
+    // }),
+  ],
+  async (req, res) => {
+    let error = validationResult(req);
+    // console.log(error);
+    if (!error.isEmpty()) {
+      res.json("Enter the correct Information");
+    }
+    // const[emailtocheck,passwordtocheck]=req.body;
+    let emailtocheck = req.body.email;
+    let passwordtocheck = req.body.password;
+    let check = await department.findOne({ email: emailtocheck });
+    if (!check) {
+      return res.json({ Error: "Please login with valid credentials" });
+    } else {
+      if (await bcrypt.compare(passwordtocheck, check.password)) {
+        let latestcode = JWT.sign(check.id, "mynameisnavneetraj");
+        return res.json({
+          Message: "Your Succesfully Login",
+          AUTH_TOKEN: latestcode,
+          NAME: check.name,
+        });
+      } else {
+        return res.json({
+          message: "Bap se bakchodi",
+          advice: "Account banaale hack mat kar ",
+        });
+      }
+    }
+  }
+);
+//process will be extracting id from Auth token to get User detail trying to fetch his own details
+router.get("/getdepartmentdetails", async (req, res) => {
+  try {
+    // console.log(req.user);
+    let fetchinguserid = req;
+    const token = req.header("auth_token");
+    // console.log("token "+token);
+    if (!token) {
+      return res.status(401).send("Access Denied");
+    }
+    //extracting id of user from token
+    let data = JWT.verify(token, "mynameisnavneetraj");
+    // console.log("data "+data);
+    // eyJhbGciOiJIUzI1NiJ9.NjFlODQ1YmIyYzg3NTU4MzVhYmVkZmRm.AZTvK6kzTwq7D_k06dNwWuQK28nI9xmC9EWbfo_Sx3M
+    const fetcheddepartment = await department.findById(data);
+    // console.log("nv--"+fetcheduser.id);
+    // console.log(`nishant ${fetcheduser}`);
+    res.send(fetcheddepartment);
+  } catch (error) {
+    res.status(500).send(`Internal Server Error due to ${error}`);
+  }
+});
+
+//Delete a user from database
+router.delete("/deletedepartment", async (req, res) => {
+  let auth_token = req.header("auth_token");
+  if (!auth_token) {
+    return res.status(401).send("Wrong User");
+  }
+
+  let data = JWT.verify(auth_token, "mynameisnavneetraj");
+  let olddepartment = await department.findByIdAndDelete(data);
+  if (!olddepartment) {
+    return res.send("Department not exists");
+  }
+  res.status(400).json({ result: "Departmentdeleted", departmentDeleted: olddepartment });
+});
 module.exports = router;
 
